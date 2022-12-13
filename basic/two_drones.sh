7ab17f7bd023f31d1dd3ca9f3051acd34615f384
KueiKao <r08921109@ntu.edu.tw>
2022-12-13 14:58:32 +0800

    structure for two drones. tested ok

--- basic/two_drones.sh
+++ basic/two_drones.sh
@@ -2,21 +2,25 @@
 # =============================================================
 # [change config]
 # =============================================================
-AP_ID="3"
 IS_DRIVER="true"
 IS_CAMERA_VIZ="true"
-IS_RECORD="true"
+IS_RECORD="false"
 IS_ACTION="true"
 IS_PERCEPTION="true"
-DIR_NS="/home/kuei/Documents/records/single_lift_middle"
-BAG_NAME="Desire-30deg_Kp-2-2-3_no-fixing_apid-3_2nd"
+DIR_NS="/home/kuei/Documents/records/two_lift_corner"
+BAG_NAME="apid-2-4_desire-30deg_Kp-2-2-3_2nd"
+
+TELLO_1_NS="tello_601"
+TELLO_2_NS="tello_C"
+TELLO1_AP_ID="2"
+TELLO2_AP_ID="4"
 
 if [ $IS_RECORD == "true" ]
 then
     mkdir -p $DIR_NS
 fi
 
-roslaunch tello_driver single_drone.launch \
+roslaunch tello_driver two_drone.launch \
     dir_ns:=$DIR_NS \
     bag_name:=$BAG_NAME \
     is_driver:=$IS_DRIVER \
@@ -24,4 +28,7 @@ roslaunch tello_driver single_drone.launch \
     is_camera_viz:=$IS_CAMERA_VIZ \
     is_action:=$IS_ACTION \
     is_perception:=$IS_PERCEPTION \
-    ap_id:=$AP_ID
+    tello_ns1:=$TELLO_1_NS \
+    tello_ns2:=$TELLO_2_NS \
+    tello1_ap_id:=$TELLO1_AP_ID \
+    tello2_ap_id:=$TELLO2_AP_ID

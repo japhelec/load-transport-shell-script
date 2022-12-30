47964af4dc20dca351c9f2934ccc630970d1529b
KueiKao <r08921109@ntu.edu.tw>
2022-12-30 15:55:19 +0800

    compress image

--- basic/single_drone_launch.sh
+++ basic/single_drone_launch.sh
@@ -5,6 +5,7 @@ TELLO_NS="tello_601"
 
 IS_DRIVER="true"
 IS_CAMERA_VIZ="true"
+IMG_TYPE="image_raw"
 IFNAME="wlp0s20f3"
 
 
@@ -12,5 +13,6 @@ IFNAME="wlp0s20f3"
 roslaunch tello_driver single_drone_launch.launch \
     is_driver:=$IS_DRIVER \
     is_camera_viz:=$IS_CAMERA_VIZ \
+    img_type:=$IMG_TYPE \
     ifname:=$IFNAME \
     tello_ns:=$TELLO_NS
